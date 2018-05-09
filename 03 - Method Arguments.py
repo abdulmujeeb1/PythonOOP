ba84e8d31c8d68b0c1d8288eb8542df189455561
mujeebishaque <nhawk1100@gmail.com>
2018-05-10 00:24:05 +0500

    method arguments and kwargs

---
+++ 03 - Method Arguments.py
@@ -0,0 +1,19 @@
+
+
+class SomeClassName:
+    longName = False
+    name = "dhadoo"
+    def __init__ (self, name, longName = False, **kwargs):
+        # kwargs = keywords argument    
+        self.longName = longName
+        self.name = name
+
+    def BetterName(self, userThoughts):
+        if (self.longName): 
+            return False
+        return True
+
+
+
+stackObject = SomeClassName('Zeus', True)
+print(stackObject.name)
