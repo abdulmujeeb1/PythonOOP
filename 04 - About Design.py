7e1d32d925be9fc69d3fc8ce3113896808b28d35
mujeebishaque <nhawk1100@gmail.com>
2018-05-11 21:22:22 +0500

    YAGNI

---
+++ 04 - About Design.py
@@ -0,0 +1,10 @@
+# PIEA,s
+# 
+
+# All the function would really mess up things
+# why not create certain functions that represent certain Noun/Object?
+# OOP saves headache of going through millions of functions and several others things. 
+# put those funtions in a class. 
+
+# YAGNI => You ain't gonna need it.
+# Don't think to much, keep it to point and to what you are building. Keep it simple, lean and mean.
